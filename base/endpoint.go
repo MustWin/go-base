1b08ef2dedf6481160fd1f2a41da8aa140ff5904
robby <robertapolana@gmail.com>
2015-10-26 15:49:41 -0400

    Updated healthcheck to be congruent with new go-kit transport/http/server api.

--- base/endpoint.go
+++ base/endpoint.go
@@ -1,54 +1,54 @@
 package base
 
-import(
-  httptransport "github.com/go-kit/kit/transport/http"
+import (
+	httptransport "github.com/go-kit/kit/transport/http"
 )
 
 type Endpoint interface {
-  GetRoute() string
-  GetMethod() string
-  GetDescription() string
-  GetParameters() interface{}
-  GetBody() interface{}
-  GetResponses() []Response
-  GetHandler() httptransport.Server
+	GetRoute() string
+	GetMethod() string
+	GetDescription() string
+	GetParameters() interface{}
+	GetBody() interface{}
+	GetResponses() []Response
+	GetHandler() *httptransport.Server
 }
 
 type BaseEndpoint struct {
-  Route string
-  Method string
-  Description string
-  Parameters interface{}
-  Body interface{}
-  Responses []Response
-  Handler httptransport.Server
+	Route       string
+	Method      string
+	Description string
+	Parameters  interface{}
+	Body        interface{}
+	Responses   []Response
+	Handler     *httptransport.Server
 }
 
 func (ref *BaseEndpoint) GetRoute() string {
-  return ref.Route
+	return ref.Route
 }
 func (ref *BaseEndpoint) GetMethod() string {
-  return ref.Method
+	return ref.Method
 }
 func (ref *BaseEndpoint) GetDescription() string {
-  return ref.Description
+	return ref.Description
 }
 func (ref *BaseEndpoint) GetParameters() interface{} {
-  return ref.GetParameters
+	return ref.GetParameters
 }
 func (ref *BaseEndpoint) GetBody() interface{} {
-  return ref.Body
+	return ref.Body
 }
-func (ref *BaseEndpoint) GetResponses() []Response{
-  return ref.Responses
+func (ref *BaseEndpoint) GetResponses() []Response {
+	return ref.Responses
 }
-func (ref *BaseEndpoint) GetHandler() httptransport.Server {
-  return ref.Handler
+func (ref *BaseEndpoint) GetHandler() *httptransport.Server {
+	return ref.Handler
 }
 
 var Endpoints []Endpoint
-func RegisterEndpoints(endpoints ...Endpoint) {
-  // TODO: Prevent duplicate additions
-  Endpoints = append(Endpoints, endpoints...)
-}
 
+func RegisterEndpoints(endpoints ...Endpoint) {
+	// TODO: Prevent duplicate additions
+	Endpoints = append(Endpoints, endpoints...)
+}
